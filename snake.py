b57cd760f0f654a81f1ad98f186a17071747c22c
Akash Rahman <akashrahman.me@gmail.com>
2024-07-16 12:47:22 +0600

    snake direction

--- snake.py
+++ snake.py
@@ -13,6 +13,10 @@ class SnakeGame:
       self.food_color = (255, 0, 0)
       self.snake_color = (0, 255, 0)
 
+      self.direction = "RIGHT"
+      self.snake_length = 5
+      self.snake_speed = 10
+
       pg.display.init()
       pg.font.init()
       pg.mixer.init()
@@ -21,42 +25,29 @@ class SnakeGame:
       pg.display.set_caption("Snake Game")
 
    
-   def mainloop(self):
+   def run(self):
       while True:
-         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-               pygame.quit()
+         for event in pg.event.get():
+            if event.type == pg.QUIT:
+               pg.quit()
                sys.exit()
-            if event.type == pygame.KEYDOWN:
-               if event.key == pygame.K_UP:
-                  
+            if event.type == pg.KEYDOWN:
+               if event.key == pg.K_UP:
+                  print("Key up is press")
+               if event.key == pg.K_DOWN:
+                  print("Key down is press")
+               if event.key == pg.K_LEFT:
+                  print("Key left is press")
+               if event.key == pg.K_RIGHT:
+                  print("Key right is press")
 
-         self.screen.fill(self.bg_color)
+         self.screen.fill((0, 0, 0))
 
-         # Draw straight lines
-         self.draw_straight_lines()
+         # Draw Snake
+         for i in range(self.snake_length):
+            pg.draw.rect(self.screen, self.snake_color, (i * self.food_size, 0, self.food_size, self.food_size))
+            
+         pg.display.flip()
 
-         # Draw Guti
-         for color, identifier, index in gutis_assign:
-            position = self.index_to_position(index)
-            self.draw_guti(position, color)
-
-            if selected_guti_identifier == identifier:
-              # self.move_eligiblity(selected_guti_identifier)
-              self.move_fight(selected_guti_identifier)
-
-         # Display scores
-         for index, pleyar in enumerate(self.board):
-            score = 16 - len(pleyar)
-            pos = (self.space, self.space + self.line_gap / 2) if index == 1 else\
-               (self.screen_w - self.space - 56, self.screen_h - self.space - 10 - (self.line_gap / 2))
-            self.draw_text(f"Score: {score}", pos)
-
-         if is_game_over:
-            pygame.draw.circle(self.screen, self.protagonist_color if is_game_over == 0 else self.antagonist_color, (self.screen_w / 2, self.screen_h / 2), 100)
-            font = pygame.font.Font(None, 24)
-            text_surface = font.render("Game Over", True, (255, 255, 255))
-            text_rect = text_surface.get_rect(center=(self.screen_w / 2, self.screen_h / 2))
-            self.screen.blit(text_surface,text_rect)
-
-         pygame.display.flip()
+game = SnakeGame()
+game.run()
